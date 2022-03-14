a5f08fee1eafe395338f3e0e8c95ef5d3b13c701
amirmahdi.daraie <79265218+amir6909mahdi@users.noreply.github.com>
2022-03-14 17:59:31 +0330

    Add files via upload

---
+++ Recorder/Recorder/Models/Record.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Recorder.Models
+{
+    public class Record
+    {
+        private int id;
+        private int randomInt;
+        private string time;
+        private static Random random = new Random();
+
+
+        public Record()
+        {
+            // Id = StaticId++;
+            // this line automatically run by SQL with this syntax: Identity (1, 1)
+            RandomInt = random.Next(1, 1000);
+            Time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public int RandomInt
+        {
+            get => randomInt;
+            set => randomInt = value;
+        }
+
+
+        [Column(TypeName = "varchar(20)")]
+        public string Time
+        {
+            get => time;
+            set => time = value;
+        }
+
+    }
+}
