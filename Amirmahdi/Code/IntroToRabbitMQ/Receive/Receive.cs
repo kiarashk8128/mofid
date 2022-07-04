5f342b36f445f33e1bd776f7674b0f18d397004d
amirmahdi.daraie <79265218+amir6909mahdi@users.noreply.github.com>
2022-07-04 09:47:20 +0430

    Add files via upload

---
+++ Amirmahdi/Code/IntroToRabbitMQ/Receive/Receive.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "TimeLogger",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Thread.Sleep(500);
+                string path = @"../../../Records.txt";
+                string text = $"message: {message}, acknowledge time: {DateTime.Now.ToString()}";
+                File.AppendAllText(path, text + Environment.NewLine);
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                Console.WriteLine($"Consumer Received {message}");
+            };
+            channel.BasicConsume(queue: "TimeLogger",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Amirmahdi/Code/IntroToRabbitMQ/Send/Send.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "TimeLogger",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            while (true)
+            {
+                string message = Chronometer();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "TimeLogger",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Producer Sent {message}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    public static string Chronometer()
+    {
+        return DateTime.Now.ToString();
+    }
+}
