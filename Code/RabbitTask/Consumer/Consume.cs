a14c8c49d6da7fc50ac31c2b93011f2becaabd3a
amirmahdi.daraie <79265218+amir6909mahdi@users.noreply.github.com>
2022-07-04 11:05:52 +0430

    Add files via upload

---
+++ Code/RabbitTask/Consumer/Consume.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public class Consume
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Messenger", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                
+                Console.WriteLine(" Consumer Waiting for messages.");
+
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    Thread.Sleep(500);
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                     
+                    Console.WriteLine(" Consumer Received message : {0}",message);
+                    
+                };
+
+                channel.BasicConsume(queue: "Messenger", autoAck: true, consumer: consumer);
+                Console.WriteLine("Acknowleged finished at : {0} ",DateTime.Now.ToString());
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
